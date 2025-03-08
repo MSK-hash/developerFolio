9994903ffd1c1f070a91afa5246ac8412805caa0
MSK-hash <115076194+MSK-hash@users.noreply.github.com>
2025-03-08 02:21:15 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saikiran Mamidala",
+  username: "Saikiran Mamidalaa",
   title: "Hi all, I'm Saikiran",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
